136f698eb8c0264dece85707acf1f91d89b916be
bashar4980 <sagor.ahmed3669@gmail.com>
2022-10-11 22:16:51 +0600

    header is created

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from 'react-bootstrap/Nav';
+import './Header.css'
+
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand  className="fw-bold text-primary">Quizadda</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">Statistics</Nav.Link>
+            <Nav.Link href="/">Blog</Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
+           
+            
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
